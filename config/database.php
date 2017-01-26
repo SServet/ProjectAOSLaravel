1644274eb48ab1996d31d45bc2e81b3836856513
Enetikk <i12001@student.htlwrn.ac.at>
2017-01-26 10:30:49 +0100

    bugfixx

--- config/database.php
+++ config/database.php
@@ -52,13 +52,13 @@ return [
             'prefix' => '',
         ],
 
-        'mysql' => [
+            'mysql' => [
             'driver' => 'mysql',
-            'host' => 'localhost',
-            'port' => '3306',
-            'database' =>'beispiel',
-            'username' => 'root',
-            'password' => 'root',
+            'host' => env('DB_HOST', 'localhost'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
