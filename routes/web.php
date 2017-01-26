f41a47d224fa09a4e6307f13c00ec7abd28c79a9
XeroxC <caner_demirbag@hotmail.de>
2017-01-26 11:48:39 +0100

    Merge Fixed

--- routes/web.php
+++ routes/web.php
@@ -12,18 +12,3 @@
 */
 
 Route::get('/', ['uses' => 'IndexController@showIndex']);
-
-
-Route::get('/Arbeitsschein', ['uses' => 'ArbeitsscheinController@showArbeitsschein']);
-Route::get('/Arbeitsschein_Hinzufuegen', ['uses' => 'ArbeitsscheinController@showArbeitsscheinHinzufuegen']);
-Route::get('/Arbeitsscheine_Uebersicht', ['uses' => 'ArbeitsscheinController@showArbeitsscheinUebersicht']);
-
-Route::get('/Projekte', ['uses' => 'ProjektController@showProjekt']);
-Route::get('/Projekt_Hinzufuegen', ['uses' => 'ProjektController@showProjektHinzufuegen']);
-Route::get('/Projekte_Uebersicht', ['uses' => 'ProjektController@showProjektUebersicht']);
-
-Route::get('/Tickets', ['uses' => 'TicketController@showTicket']);
-Route::get('/Ticket_Hinzufuegen', ['uses' => 'TicketController@showTicketHinzufuegen']);
-Route::get('/Tickets_Uebersicht', ['uses' => 'TicketController@showTicketUebersicht']);
-
-Route::get('/Einstellungen', ['uses' => 'EinstellungenController@showEinstellungen']);
