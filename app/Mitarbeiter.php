71cb90b77353c35bd04f58ea17c61da45491e0a1
SServet <servet.simsek@outlook.com>
2017-02-23 09:57:24 +0100

    Seeder erstellt

--- app/Mitarbeiter.php
+++ app/Mitarbeiter.php
@@ -5,18 +5,22 @@ namespace App;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Authenticatable
+class Mitarbeiter extends Authenticatable
 {
     use Notifiable;
 
+    protected $table = "Mitarbeiter";
+    
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'MID', 'Benutzername', 'Passwort', 'Rolle', 'Anrede', 'Vorname', 'Nachname', 'Land', 'PLZ', 'Ort', 'EMail'
     ];
+
+
 
     /**
      * The attributes that should be hidden for arrays.
--- database/seeds/MitarbeiterTableSeeder.php
+++ database/seeds/MitarbeiterTableSeeder.php
@@ -1,9 +1,9 @@
 <?php
 
 use Illuminate\Database\Seeder;
-use App\User as User;
+use App\Mitarbeiter as Mitarbeiter;
 
-class UsersTableSeeder extends Seeder
+class MitarbeiterTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -13,15 +13,15 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         \DB::table("Mitarbeiter")->delete();
-        User::create([
+        Mitarbeiter::create([
         	'MID' => '1',
         	'Benutzername' => 'Adi',
         	'Passwort' => '1234',
         	'Rolle' => 'Techniker',
             'Anrede' => 'Herr',
             'Vorname' => 'Enes',
-            'Nachname' => 'Adigüzel',
-            'Land' => 'Österreich',
+            'Nachname' => 'Adiguezel',
+            'Land' => 'Oesterreich',
             'PLZ' => '2700',
             'Ort' => 'Neustadt',
             'EMail' => 'i12001@student.htlwrn.ac.at',
