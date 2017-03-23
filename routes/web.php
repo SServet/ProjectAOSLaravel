e2c7875490a85b18ce6cfad2d6f7fef834656f30
Maki <Maki>
2017-03-23 11:56:21 +0100

    Authentifikation fertig

--- routes/web.php
+++ routes/web.php
@@ -11,25 +11,21 @@
 |
 */
 
-Route::get('/home', ['uses' => 'IndexController@showIndex']);
+Route::get('/', ['uses' => 'LoginController@showLogin']);
 
-
-Route::get('/Arbeitsschein', ['uses' => 'ArbeitsscheinController@showArbeitsschein']);
-Route::get('/Arbeitsschein_Hinzufuegen', ['uses' => 'ArbeitsscheinController@showArbeitsscheinHinzufuegen']);
-Route::get('/Arbeitsschein_Uebersicht', ['uses' => 'ArbeitsscheinController@showArbeitsscheinUebersicht']);
-
-Route::get('/Projekte', ['uses' => 'ProjektController@showProjekt']);
-Route::get('/Projekt_Hinzufuegen', ['uses' => 'ProjektController@showProjektHinzufuegen']);
-Route::get('/Projekte_Uebersicht', ['uses' => 'ProjektController@showProjektUebersicht']);
-
-Route::get('/Tickets', ['uses' => 'TicketController@showTicket']);
-Route::get('/Ticket_Hinzufuegen', ['uses' => 'TicketController@showTicketHinzufuegen']);
-Route::get('/Tickets_Uebersicht', ['uses' => 'TicketController@showTicketUebersicht']);
-
-Route::get('/Einstellungen', ['uses' => 'EinstellungenController@showEinstellungen']);
-
-Route::post('/SubmitProject', ['as' => 'SubmitProject', 'uses' => 'ProjektController@submit']);
-Route::post('/SubmitArbeitS', ['as' => 'SubmitArbeitS', 'uses' => 'ArbeitsscheinController@submit']);
-Route::post('/SubmitTicket', ['as' => 'SubmitTicket', 'uses' => 'TicketController@submit']);
+Route::group(['middleware' => 'auth'], function(){
+	Route::get('/', ['uses' => 'IndexController@showIndex']);
+	Route::get('/home', ['uses' => 'IndexController@showIndex']);
+	Route::get('/Arbeitsschein', ['uses' => 'ArbeitsscheinController@showArbeitsschein']);
+	Route::get('/Arbeitsschein_Hinzufuegen', ['uses' => 'ArbeitsscheinController@showArbeitsscheinHinzufuegen']);
+	Route::get('/Arbeitsschein_Uebersicht', ['uses' => 'ArbeitsscheinController@showArbeitsscheinUebersicht', 'middleware' => 'auth']);
+	Route::get('/Projekte', ['uses' => 'ProjektController@showProjekt']);
+	Route::get('/Projekt_Hinzufuegen', ['uses' => 'ProjektController@showProjektHinzufuegen']);
+	Route::get('/Projekte_Uebersicht', ['uses' => 'ProjektController@showProjektUebersicht']);
+	Route::get('/Tickets', ['uses' => 'TicketController@showTicket']);
+	Route::get('/Ticket_Hinzufuegen', ['uses' => 'TicketController@showTicketHinzufuegen']);
+	Route::get('/Tickets_Uebersicht', ['uses' => 'TicketController@showTicketUebersicht']);
+	Route::get('/Einstellungen', ['uses' => 'EinstellungenController@showEinstellungen']);
+});
 
 Auth::routes();
