c6e16f5c1ad6ed72d95f71a77732089024133aa0
XeroxC <caner_demirbag@hotmail.de>
2017-06-21 13:10:04 +0200

    Fixes bei ArbeitsscheinProjet

--- app/Http/Controllers/ProjektController.php
+++ app/Http/Controllers/ProjektController.php
@@ -23,187 +23,221 @@ class ProjektController extends Controller
         return view('apHinzufuegen');
     }
 
-	public function showProjektUebersicht(){
-		return view('pUebersicht');
-	}
+    public function showProjektUebersicht(){
+      return view('pUebersicht');
+  }
 
-    public function showProjektEdit(){
-        return view('pEdit');
-    }
+  public function showProjektEdit(){
+    return view('pEdit');
+}
 
-    public function closeProjekt(Request $request){
-        $projekt = Projekte::where('pid', '=', $request->get('pid'))->first();
-        $projekt->finishedOn = date('Y-m-d');
-        $projekt->save();
+public function closeProjekt(Request $request){
+    $projekt = Projekte::where('pid', '=', $request->get('pid'))->first();
+    $projekt->finishedOn = date('Y-m-d');
+    $projekt->save();
 
-        return redirect('Projekte');
-    }
+    return redirect('Projekte');
+}
 
-    public function settleProjekt(Request $request){
-        $projekt = Projekte::where('pid', '=', $request->get('pid'))->first();
-        $projekt->settledOn = date('Y-m-d');
-        $projekt->finishedOn = date('Y-m-d');
-        $projekt->save();
+public function settleProjekt(Request $request){
+    $projekt = Projekte::where('pid', '=', $request->get('pid'))->first();
+    $projekt->settledOn = date('Y-m-d');
+    $projekt->finishedOn = date('Y-m-d');
+    $projekt->save();
 
-        return redirect('Projekte');
-    }
+    return redirect('Projekte');
+}
 
-    public function submitAProjekt(Request $request){
-        $AProjekt = new Arbeitsscheinprojekt;
+public function submitAProjekt(Request $request){
+    $AProjekt = new Arbeitsscheinprojekt;
+    $artikel = new Artikel;
+    $cache;
 
-        $AProjekt->pid = $request->get('pid');
-        $AProjekt->mid = $request->get('mid');
-        $AProjekt->artid = $request->get('aid');
-        $AProjekt->artAnz = $request->get('artAnz');
-
-        $AProjekt->description = $request->get('description');
-        $AProjekt->ttid = explode('.',$request->get('ttid'))[0];
-        $AProjekt->tkid = explode('.',$request->get('tkid'))[0];
-        $AProjekt->dateFrom = $request->get('dateFrom');
-        
-        if($request->get('dateTo') == ''){
-            $AProjekt->dateTo = null;            
-        } else {
-            $AProjekt->dateTo = $request->get('dateTo');
-        }
-
-        if($request->get('timeFrom') == ''){
-           $AProjekt->timeFrom  = null;
-            
-        } else {
-           $AProjekt->timeFrom = $request->get('timeFrom');
-        }
-
-        if($request->get('timeTo') == ''){
-            $AProjekt->timeTo  = null;
-            
-        } else {
-            $AProjekt->timeTo = $request->get('timeTo');
-        }
-
-        if($request->get('billedTime') == ''){
-            $AProjekt->billedTime  = null;
-            
-        } else {
-           $AProjekt->billedTime = $request->get('billedTime');
-        }
-
-        if($request->get('kulanzzeit') == ''){
-           $AProjekt->kulanzzeit  = null;
-            
-        } else {
-            $AProjekt->kulanzzeit = $request->get('kulanzzeit');
-        }
-
-        if($request->get('kulanzgrund') == ''){
-           $AProjekt->kulanzgrund  = null;
-            
-        } else {
-            $AProjekt->kulanzgrund = $request->get('kulanzgrund');
-        } 
-        if($request->get('kulanzzeit') == ''){
-            $AProjekt->kulanzgrund  = null;
-            
-        } else {
-            $AProjekt->kulanzgrund = $request->get('kulanzzeit');
-        }
-        $AProjekt->save();
-
-        /* return \App::call('App\Http\Controllers\AP_ItemController@pdfview'); */
-
-        return redirect('Projekte');
+    if(count(explode('.',$request->get('artid'))) != 1){
+        $artikel->articlename = explode('.',$request->get('artid'))[1];
+        $artikel->artid = explode('.',$request->get('artid'))[0];
+        $artikel->agid=14;
+        $cache = explode('.',$request->get('artid'))[0];
 
     }
-
-	public function submit(Request $request){
-
-		$projekt = new Projekte;
-        $artikel = new Artikel;
-        $cache;
-
-        if(count(explode('.',$request->get('artid'))) != 1){
-            $artikel->articlename = explode('.',$request->get('artid'))[1];
-            $artikel->artid = explode('.',$request->get('artid'))[0];
-            $artikel->agid=14;
-            $cache = explode('.',$request->get('artid'))[0];
-
-        }
-        else{
-            $artikel->artid = $request->get('artid');
-            $artikel->articlename = $request->get('artid');
-            $artikel->agid=14;
-            $artikel->save();
-            $cache = $request->get('artid');
-            }
-
-        $projekt->kid = explode('.',$request->get('kid'))[0];
-        $projekt->mid = $request->get('mid');
-        $projekt->label = $request->input('label');
-        $projekt->description = $request->get('description');
-        $projekt->projectType = $request->get('projectType');
-       
-        $projekt->dateOfOrder = $request->get('dateOfOrder');
-        $projekt->artid = $cache;
-        $projekt->artAnz = $request->get('artAnz');
-        
-        if($request->get('projectVolume') == ''){
-              $projekt->projectVolume = null;
-            
-        } else {
-             $projekt->projectVolume = $request->get('projectVolume');
-        }
-
-
-        if($request->get('finishedOn') == ''){
-             $projekt->finishedOn = null;
-            
-        } else {
-            $projekt->finishedOn = $request->get('finishedOn');
-        }
-        
-        if( $request->get('settledOn') == ''){
-            $projekt->settledOn = null;
-            
-        } else {
-            $projekt->settledOn = $request->get('settledOn');
-        }
-
-        $projekt->save();
-
-        return redirect('Projekte');
+    else{
+        $artikel->artid = $request->get('artid');
+        $artikel->articlename = $request->get('artid');
+        $artikel->agid=14;
+        $artikel->save();
+        $cache = $request->get('artid');
     }
 
-    public function submitEditProjekt(Request $request){
-        $projekt = Projekte::find($request->get('pid'));
-        $projekt->pid = $request->get('pid');
-        $projekt->kid = $request->get('kid');
-        $projekt->mid = $request->get('mid');   
-        $projekt->artid = explode('.',$request->get('artid'))[0];
-        $projekt->artAnz = $request->get('artAnz');
-       
+    $AProjekt->pid = $request->get('pid');
+    $AProjekt->mid = $request->get('mid');
+    $AProjekt->artAnz = $request->get('artAnz');
 
+    $AProjekt->description = $request->get('description');
+    $AProjekt->ttid = explode('.',$request->get('ttid'))[0];
+    $AProjekt->tkid = explode('.',$request->get('tkid'))[0];
+    $AProjekt->dateFrom = $request->get('dateFrom');
 
-        $projekt->label = $request->input('label');
-        $projekt->description = $request->get('description');
-        $projekt->dateOfOrder = $request->get('dateOfOrder');
-
-        if($request->get('finishedOn') == ''){
-            $projekt->finishedOn = null;
-            
-        } else {
-            $projekt->finishedOn = $request->get('finishedOn');
-        }
-        
-        if( $request->get('settledOn') == ''){
-            $projekt->settledOn = null;
-            
-        } else {
-            $projekt->settledOn = $request->get('settledOn');
-        }
-
-
-        $projekt->save();
-
-        return redirect('Projekte');
+    if($request->get('dateTo') == ''){
+        $AProjekt->dateTo = null;            
+    } else {
+        $AProjekt->dateTo = $request->get('dateTo');
     }
+
+    if($request->get('timeFrom') == ''){
+     $AProjekt->timeFrom  = null;
+
+ } else {
+     $AProjekt->timeFrom = $request->get('timeFrom');
+ }
+
+ if($request->get('timeTo') == ''){
+    $AProjekt->timeTo  = null;
+
+} else {
+    $AProjekt->timeTo = $request->get('timeTo');
+}
+
+if($request->get('billedTime') == ''){
+    $AProjekt->billedTime  = null;
+
+} else {
+ $AProjekt->billedTime = $request->get('billedTime');
+}
+
+if($request->get('kulanzzeit') == ''){
+ $AProjekt->kulanzzeit  = null;
+
+} else {
+    $AProjekt->kulanzzeit = $request->get('kulanzzeit');
+}
+
+if($request->get('kulanzgrund') == ''){
+ $AProjekt->kulanzgrund  = null;
+
+} else {
+    $AProjekt->kulanzgrund = $request->get('kulanzgrund');
+} 
+if($request->get('kulanzzeit') == ''){
+    $AProjekt->kulanzgrund  = null;
+
+} else {
+    $AProjekt->kulanzgrund = $request->get('kulanzzeit');
+}
+if($request->get('artid') == ''){
+    $AProjekt->artid = null;
+}else{
+    $AProjekt->artid = $cache;
+}
+$AProjekt->save();
+
+
+/* return \App::call('App\Http\Controllers\AP_ItemController@pdfview'); */
+
+return redirect('Projekte');
+
+}
+
+public function submit(Request $request){
+
+  $projekt = new Projekte;
+  $artikel = new Artikel;
+  $cache;
+
+  if(count(explode('.',$request->get('artid'))) != 1){
+    $artikel->articlename = explode('.',$request->get('artid'))[1];
+    $artikel->artid = explode('.',$request->get('artid'))[0];
+    $artikel->agid=14;
+    $cache = explode('.',$request->get('artid'))[0];
+
+}
+else{
+    $artikel->artid = $request->get('artid');
+    $artikel->articlename = $request->get('artid');
+    $artikel->agid=14;
+    $artikel->save();
+    $cache = $request->get('artid');
+}
+
+$projekt->kid = explode('.',$request->get('kid'))[0];
+$projekt->mid = $request->get('mid');
+$projekt->label = $request->input('label');
+$projekt->description = $request->get('description');
+$projekt->projectType = $request->get('projectType');
+
+$projekt->dateOfOrder = $request->get('dateOfOrder');
+$projekt->artid = $cache;
+$projekt->artAnz = $request->get('artAnz');
+
+if($request->get('projectVolume') == ''){
+  $projekt->projectVolume = null;
+
+} else {
+   $projekt->projectVolume = $request->get('projectVolume');
+}
+
+
+if($request->get('finishedOn') == ''){
+   $projekt->finishedOn = null;
+
+} else {
+    $projekt->finishedOn = $request->get('finishedOn');
+}
+
+if( $request->get('settledOn') == ''){
+    $projekt->settledOn = null;
+
+} else {
+    $projekt->settledOn = $request->get('settledOn');
+}
+
+if($request->get('artid') == ''){
+    $projekt->artid = null;
+}else{
+    $projekt->artid = $cache;
+}
+
+if($request->get('artAnz') == ''){
+    $projekt->artAnz = null;
+}else{
+    $projekt->artAnz = $request->get('artAnz');
+}
+
+$projekt->save();
+
+return redirect('Projekte');
+}
+
+public function submitEditProjekt(Request $request){
+    $projekt = Projekte::find($request->get('pid'));
+    $projekt->pid = $request->get('pid');
+    $projekt->kid = $request->get('kid');
+    $projekt->mid = $request->get('mid');   
+    $projekt->artid = explode('.',$request->get('artid'))[0];
+    $projekt->artAnz = $request->get('artAnz');
+
+
+
+    $projekt->label = $request->input('label');
+    $projekt->description = $request->get('description');
+    $projekt->dateOfOrder = $request->get('dateOfOrder');
+
+    if($request->get('finishedOn') == ''){
+        $projekt->finishedOn = null;
+
+    } else {
+        $projekt->finishedOn = $request->get('finishedOn');
+    }
+
+    if( $request->get('settledOn') == ''){
+        $projekt->settledOn = null;
+
+    } else {
+        $projekt->settledOn = $request->get('settledOn');
+    }
+
+
+    $projekt->save();
+
+    return redirect('Projekte');
+}
 }
--- resources/views/apHinzufuegen.blade.php
+++ resources/views/apHinzufuegen.blade.php
@@ -118,21 +118,23 @@
           <input type="text" id="Mitarbeiter-Referenz" class="form-control input-lg" name="mitarbeiter_name" value="{{$user->firstname . ' ' . $user->lastname}}" readonly>
         </td>
       </tr>
+      <tr id="artikelTR">
+      </tr>
       <tr>
-       <td><p class="inputLabels">Artikel</p></td>
-       <td>
-        <select data-placeholder="Artikel auswählen..." id="artikel_select" class="chosen-select" style="width:350px;" tabindex="2" name="aid">
-          <option value=""></option>
-          @foreach ($artikel as $art)
-          <option>{{$art->artid}}. {{$art->articlename}}</option>
-          @endforeach
-        </select>
-      </td>
-    </tr>
-    <tr>
-      <td><p class="inputLabels">Artikelanzahl</p></td>
-      <td><input type="number" class="form-control input-lg" min="0" value="1" name="artAnz"></td>
-    </tr>
+        <td><p class="inputLabels">Artikel</p></td>
+        <td>
+          <select data-placeholder="Artikel auswählen..." id="artikel_select" class="chosen-select form-control input-lg" style="width:350px; height: 400px;" tabindex="2" name="artid">
+            <option value="" id="inputArtikel" onchange="newArtikel()"></option>
+            @foreach ($artikel as $art)
+            <option>{{$art->artid}}. {{$art->articlename}}</option>
+            @endforeach
+          </select>
+        </td>
+      </tr>
+      <tr>
+        <td><p class="inputLabels">Artikelanzahl</p></td>
+        <td><input type="number" class="form-control input-lg" min="0" value="1" name="artAnz"></td>
+      </tr>
     <tr>
       <td><p class="inputLabels">Beschreibung</p></td>
       <td><textarea id="Beschreibung" class="form-control input-lg" style="" name="description"></textarea></td>
@@ -244,6 +246,28 @@
   });
   
   $(".chosen-select").chosen();
+});
+ var chosen = $("#artikel_select").chosen().data('chosen');
+chosen.container.bind('keydown', function (e) {
+    if(e.which==187){
+      window.searchNow=true;
+    }else{
+      window.searchNow=false;
+    }
+});
+
+$("#artikel_select").on('chosen:no_results', function(e, params) {
+   var artikel = params.chosen.search_results[0].textContent.match(/No results match "(.+)"/)[1];
+  
+  if(window.searchNow){
+    $("#artikel_select").append('<option>'+artikel.slice(0,-1)+'</option>').trigger("chosen:updated");
+      
+       $("#artikel_select").on('change', function(e) {
+        $("#artikel").val(artikel);
+        if($("#artikelTR td").length == 0)
+          $("#artikelTR").append('<td><p class="inputLabels">ArtikelNummer</p></td><td><input type="text" class="form-control input-lg" name="artid"></td>');
+      });
+  }
 });
 </script>
 
